4a83a8c7203db3f3e12555339ed71db695ba80c1
Shan <slu5@wellesley.edu>
2018-02-04 16:58:21 -0500

    debug env var

--- routes/routes.js
+++ routes/routes.js
@@ -29,10 +29,11 @@ router.post("/message", function(req, res) {
   console.log("DATA", data);
     // setup email data with unicode symbols
   var smtpTransport = nodemailer.createTransport(process.env.TRANSPORTER);
+  console.log("ENV", process.env.TRANSPORTER);
   let mailOptions = {
       from: req.body.name + '<wellesleywcc@gmail.com>', // sender address
-      to: 'cs-club-eboard@wellesley.edu',
-      // to: "slu5@wellesley.edu",
+      // to: 'cs-club-eboard@wellesley.edu',
+      to: "slu5@wellesley.edu",
       subject: 'Message for CS Club', // Subject line
       text: 'You received a new message from ' + req.body.name + ':\n' + req.body.message + '\n Contact her at ' + req.body.email, // plain text body
       html: '<p>You received a new message from ' + req.body.name + ':</p><p>' + req.body.message + '</p><p>Contact her at ' + req.body.email + '</p>' // html body
